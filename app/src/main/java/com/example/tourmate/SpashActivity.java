8364af8d08389ada6e13317ee1ea85f66db4f714
ruhulrahman <ruhulrahman2233@gmail.com>
2019-08-27 02:14:38 +0600

    added tourmate logo and splash animation for logo and title

--- app/src/main/java/com/example/tourmate/SpashActivity.java
+++ app/src/main/java/com/example/tourmate/SpashActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SpashActivity extends AppCompatActivity {
-
+    private ImageView logoIV;
+    private TextView tourmateTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +33,12 @@ public class SpashActivity extends AppCompatActivity {
         };
 
         thread.start();
+
+        logoIV = findViewById(R.id.logoIV);
+        tourmateTV = findViewById(R.id.tourmateTV);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logoIV.startAnimation(animation);
+        tourmateTV.startAnimation(animation);
     }
 }
