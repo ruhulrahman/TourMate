bd07bb260035aaca4558ea365e9ad26d3b9f2726
ruhulrahman <ruhulrahman2233@gmail.com>
2019-08-28 13:52:35 +0600

    Bottom navigation added and menu created

--- app/src/main/java/com/example/tourmate/activity/MainActivity.java
+++ app/src/main/java/com/example/tourmate/activity/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.tourmate.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.tourmate.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_product:
+                    replaceFragment(new ProductFragment());
+                    return true;
+                case R.id.navigation_cart:
+                    replaceFragment(new CartFragment());
+                    return true;
+                case R.id.navigation_profile:
+                    replaceFragment(new ProfileFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayoutId, fragment).commit();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        replaceFragment(new ProductFragment());;
     }
 }
