e495258ede58ce38d97f710d08c510b44dca274f
ruhulrahman <ruhulrahman2233@gmail.com>
2019-08-29 13:16:29 +0600

    expense view activity and item created

---
+++ app/src/main/java/com/example/tourmate/activity/ViewExpenseActivity.java
@@ -0,0 +1,16 @@
+package com.example.tourmate.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tourmate.R;
+
+public class ViewExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_expense);
+    }
+}
