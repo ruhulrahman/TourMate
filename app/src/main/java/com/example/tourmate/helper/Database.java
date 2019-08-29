70471010a0568da42c705cf9aac209e205b83fb4
ruhulrahman <ruhulrahman2233@gmail.com>
2019-08-29 13:59:30 +0600

    ExpenseDatabase and TourDatabase Created

--- app/src/main/java/com/example/tourmate/helper/Database.java
+++ app/src/main/java/com/example/tourmate/helper/Database.java
@@ -1,4 +1,26 @@
 package com.example.tourmate.helper;
 
-public class Database {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+    public static String DATABASE_NAME = "Tourmate.db";
+    public static int VERSION = 1;
+
+    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
---
+++ app/src/main/java/com/example/tourmate/helper/ExpenseDatabase.java
@@ -0,0 +1,53 @@
+package com.example.tourmate.helper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import androidx.annotation.Nullable;
+
+public class ExpenseDatabase extends Database {
+
+    public static String TABLE_NAME = "expense";
+    public static String COL_ID = "Id";
+    public static String COL_AMOUNT = "Amount";
+    public static String COL_PAYMENT_TYPE = "PaymentType";
+    public static String COL_DATE = "Date";
+    public static String COL_TIME = "Time";
+    public static String COL_DESCRIPTION = "Description";
+    public static String COL_COST_TYPE = "CostType";
+
+    public static String create_table = "create table "+TABLE_NAME+"(Id integer primary key, Amount Text, PaymentType Text, Date Text, Time Text, Description Text, CostType Text)";
+
+    public ExpenseDatabase(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    public ExpenseDatabase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(create_table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public long insertData(String amount, String payment,String date,String time,String desc,String cost){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_AMOUNT, amount);
+        contentValues.put(COL_PAYMENT_TYPE, payment);
+        contentValues.put(COL_DATE, date);
+        contentValues.put(COL_TIME, time);
+        contentValues.put(COL_DESCRIPTION, desc);
+        contentValues.put(COL_COST_TYPE, cost);
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        long id = sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
+        sqLiteDatabase.close();
+        return id;
+    }
+
+}
