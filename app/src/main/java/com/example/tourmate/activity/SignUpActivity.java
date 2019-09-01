cf119f7c1f22a190dd0bafade470c582e1cdc796
RHKhan <rifatkhan.cse16@gmail.com>
2019-09-01 19:15:27 +0600

    SignUp and SignIn

--- app/src/main/java/com/example/tourmate/activity/SignUpActivity.java
+++ app/src/main/java/com/example/tourmate/activity/SignUpActivity.java
@@ -1,24 +1,90 @@
 package com.example.tourmate.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tourmate.R;
-import com.example.tourmate.activity.AddExpenseActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
+    private EditText nameEt,emailEt,passwordEt;
+    private String name,email,password;
+    private Button signUpBtn;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        init();
+
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                name = nameEt.getText().toString();
+                email = emailEt.getText().toString();
+                password = passwordEt.getText().toString();
+
+                signUpRegister(name,email,password);
+            }
+        });
     }
 
-    public void LoginClick(View view) {
+    public void loginClick(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
+
+    private void signUpRegister(String name, String email, String password) {
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(SignUpActivity.this,"Success",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignUpActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+
+    private void init() {
+        nameEt = findViewById(R.id.nameEtId);
+        emailEt = findViewById(R.id.emailEtId);
+        passwordEt = findViewById(R.id.passwordEtId);
+        signUpBtn = findViewById(R.id.signUpBtnId);
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+    }
+
+
+
+
 }
